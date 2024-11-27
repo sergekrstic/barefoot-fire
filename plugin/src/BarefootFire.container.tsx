1a2d490ca2172df331556de3c336d4dfedb815f5
Serge Krstic <srdjankrstic@gmail.com>
2024-11-27 13:52:58 +1100

    Persist `react-query` data to improve experience with pre-loaded dashboard

--- plugin/src/BarefootFire.container.tsx
+++ plugin/src/BarefootFire.container.tsx
@@ -1,14 +1,42 @@
 import { StrictMode } from 'react'
 import { Root, createRoot } from 'react-dom/client'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider, QueryKey } from '@tanstack/react-query'
+import { persistQueryClient } from '@tanstack/react-query-persist-client'
+import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister'
 import { ItemView, WorkspaceLeaf } from 'obsidian'
+// import { compress, decompress } from 'lz-string'
 
 import { BarefootFireComponent } from './BarefootFire.component'
 import { BAREFOOT_FIRE_VIEW_TYPE } from './BarefootFire.defaults'
 import { BarefootFirePluginSettings } from './BarefootFire.types'
 import { useSettingsStore } from 'stores'
 
-const queryClient = new QueryClient()
+const doNotPersistQueries: QueryKey[] = []
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // gcTime: 1000 * 60 * 60 * 24 * 7, // 7 days
+    },
+  },
+})
+
+persistQueryClient({
+  // @ts-ignore
+  queryClient: queryClient,
+  persister: createSyncStoragePersister({
+    storage: window.localStorage,
+    // serialize: (data) => compress(JSON.stringify(data)),
+    // deserialize: (data) => JSON.parse(decompress(data)),
+  }),
+  maxAge: Infinity,
+  hydrateOptions: {},
+  dehydrateOptions: {
+    shouldDehydrateQuery: ({ queryKey }) => {
+      return !doNotPersistQueries.includes(queryKey)
+    },
+  },
+})
 
 export class BarefootFireContainer extends ItemView {
   root: Root | null = null
