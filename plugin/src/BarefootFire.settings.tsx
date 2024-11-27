ab1017b2ec2e0cf7c19582a7b859a22c09db73c2
Serge Krstic <srdjankrstic@gmail.com>
2024-11-27 13:50:36 +1100

    Update the settings with more complete description

--- plugin/src/BarefootFire.settings.tsx
+++ plugin/src/BarefootFire.settings.tsx
@@ -1,5 +1,4 @@
 import { App, PluginSettingTab, Setting } from 'obsidian'
-// import { unstable_batchedUpdates } from 'react-dom'
 
 import { BarefootFirePlugin } from './BarefootFire.plugin'
 import { useSettingsStore } from 'stores'
@@ -17,9 +16,36 @@ export class BarefootFireSettingTab extends PluginSettingTab {
 
     containerEl.empty()
 
+    containerEl.createEl('h2', { text: 'Barefoot F.I.R.E. Settings' })
+
+    const pocketSmithApiKeyDescription = document.createDocumentFragment()
+    pocketSmithApiKeyDescription.append(
+      'The API key to access the Pocketsmith API.',
+      pocketSmithApiKeyDescription.createEl('br'),
+      pocketSmithApiKeyDescription.createEl('br'),
+      'Visit ',
+      pocketSmithApiKeyDescription.createEl('a', {
+        href: 'https://github.com/phibr0/obsidian-dictionary#privacy',
+        text: 'Pocketsmith',
+      }),
+      ' to generate a new key.',
+      pocketSmithApiKeyDescription.createEl('br'),
+      'Click ',
+      pocketSmithApiKeyDescription.createEl('b', {
+        text: '[User Icon] > [Security & integrations] > [Manage developer keys] > [CREATE KEY]',
+      }),
+      '.',
+      pocketSmithApiKeyDescription.createEl('br'),
+      pocketSmithApiKeyDescription.createEl('br'),
+      pocketSmithApiKeyDescription.createEl('b', { text: 'Note: ' }),
+      pocketSmithApiKeyDescription.createEl('i', {
+        text: 'This key is stored in your Obsidian vault and is not shared with anyone.',
+      }),
+    )
+
     new Setting(containerEl)
       .setName('Pocketsmith API Key')
-      .setDesc('The API key to access the Pocketsmith API.')
+      .setDesc(pocketSmithApiKeyDescription)
       .addText((text) =>
         text
           .setPlaceholder('Add your API key here')
