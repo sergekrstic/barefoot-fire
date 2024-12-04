bf3de38af828693ee3baa4f4b3c1761e22c6c39b
Serge Krstic <srdjankrstic@gmail.com>
2024-12-04 15:37:44 +1100

    Refactor `useBudgets.query`

--- plugin/src/components/BudgetsList.component.tsx
+++ plugin/src/components/BudgetsList.component.tsx
@@ -2,28 +2,19 @@ import { memo } from 'react'
 import { BudgetAnalysisPackage, Category } from '@fire/pocketsmith-api'
 
 import { CollapsibleTree } from 'components'
-import { useBudgets } from 'queries'
+import { useBudgets, BudgetMap } from 'queries'
 
 export const BudgetsList = memo(function BudgetsList(): JSX.Element {
   const { data: budgets, isLoading } = useBudgets()
 
-  const rootBudgets = budgets?.filter((budget) => !budget.category?.parent_id)
-  const categories = rootBudgets?.map((budget) => budget.category as Category) || []
-  const budgetMap =
-    budgets?.reduce((acc, budget) => {
-      if (!budget.category) return acc
-      acc[budget.category.id!] = budget
-      return acc
-    }, {} as BudgetMap) || {}
-
   if (isLoading) return <p>Loading...</p>
 
-  if (!rootBudgets) return <p>No budgets found</p>
+  if (!budgets) return <p>No budgets found</p>
 
   return (
     <CollapsibleTree
-      tree={categories}
-      context={budgetMap}
+      tree={budgets.categories}
+      context={budgets.budgetMap}
       renderCollapsibleItemContent={(item: Category, budgetMap: BudgetMap) => <div>{item.title}</div>}
       renderLeafItemContent={(item: Category, budgetMap: BudgetMap) => (
         <div className="fire-section-item">
@@ -34,8 +25,6 @@ export const BudgetsList = memo(function BudgetsList(): JSX.Element {
     />
   )
 })
-
-type BudgetMap = Record<number, BudgetAnalysisPackage>
 
 function getForecastAmount(budget: BudgetAnalysisPackage): number | undefined {
   if (!budget) return undefined
--- plugin/src/queries/useBudgets.query.ts
+++ plugin/src/queries/useBudgets.query.ts
@@ -1,17 +1,40 @@
 import { useQuery, UseQueryResult } from '@tanstack/react-query'
-import { BudgetAnalysisPackage } from '@fire/pocketsmith-api'
+import { BudgetAnalysisPackage, Category } from '@fire/pocketsmith-api'
 
 import { usePocketsmithApi } from '../hooks'
 import { USER_ID } from '../BarefootFire.defaults'
 
-export function useBudgets(): UseQueryResult<BudgetAnalysisPackage[], Error> {
+export type BudgetMap = Record<number, BudgetAnalysisPackage>
+
+export interface BudgetsData {
+  rootBudgets: BudgetAnalysisPackage[]
+  categories: Category[]
+  budgetMap: BudgetMap
+}
+
+export function useBudgets(): UseQueryResult<BudgetsData, Error> {
   const api = usePocketsmithApi()
 
   return useQuery({
     queryKey: ['budgets'],
     queryFn: async () => {
       if (!api) throw new Error('No API key provided')
-      return (await api.budgeting.usersIdBudgetGet({ id: USER_ID })).data
+      const budgets = (await api.budgeting.usersIdBudgetGet({ id: USER_ID })).data
+
+      const rootBudgets = budgets?.filter((budget) => !budget.category?.parent_id)
+      const categories = rootBudgets?.map((budget) => budget.category as Category) || []
+      const budgetMap =
+        budgets?.reduce((acc, budget) => {
+          if (!budget.category) return acc
+          acc[budget.category.id!] = budget
+          return acc
+        }, {} as BudgetMap) || {}
+
+      return {
+        rootBudgets,
+        categories,
+        budgetMap,
+      }
     },
   })
 }
