742ea54f2956be45c5977efa0f7412ae45b79d10
Serge Krstic <srdjankrstic@gmail.com>
2024-12-31 13:33:41 +0800

    Test publishing storybook proper

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -4,5 +4,13 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
+  plugins: [
+    tsconfigPaths(),
+    react({
+      // jsxRuntime: 'automatic',
+    }),
+  ],
+  esbuild: {
+    jsx: 'automatic',
+  },
 })
