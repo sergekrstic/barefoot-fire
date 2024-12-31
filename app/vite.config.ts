736829204a419d3edb23e38d2b9e38dcd8155976
Serge Krstic <srdjankrstic@gmail.com>
2024-12-31 13:38:07 +0800

    Publish the app and storybook

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -4,13 +4,6 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tsconfigPaths(),
-    react({
-      // jsxRuntime: 'automatic',
-    }),
-  ],
-  esbuild: {
-    jsx: 'automatic',
-  },
+  plugins: [tsconfigPaths(), react()],
+  esbuild: { jsx: 'automatic' },
 })
