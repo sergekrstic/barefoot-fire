67333152f889ea546ed867e7d5ccead36c51bfc1
Serge Krstic <srdjankrstic@gmail.com>
2025-01-12 17:02:56 +0800

    Publish app and storybook

--- storybook/assets/ScenarioBudget.stories-BToI7wux.js
+++
@@ -1,5 +0,0 @@
-import{m as a}from"./budget.data-BXmEvIxy.js";import{S as m}from"./TimelineScrubber.component-Dmo5TZmq.js";import"./ScenarioGraph.component-CFO1VuCQ.js";import"./jsx-runtime-D_zvdyIk.js";import"./index-PahMPgEy.js";import"./colors-Cuw9ST8W.js";import"./CollapsibleTree.component-DSHTWi39.js";import"./Fire.icon-Di-S4vvp.js";import"./Header.component-BGqI9wSV.js";import"./react-resizable-panels.browser.esm-zIh8wNIw.js";const S={component:m,parameters:{layout:"fullscreen"}},r={args:{budget:a}};var o,e,t;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {
-    budget: mockCategoriesForScenarioOne
-  }
-}`,...(t=(e=r.parameters)==null?void 0:e.docs)==null?void 0:t.source}}};const O=["Default"];export{r as Default,O as __namedExportsOrder,S as default};
---
+++ storybook/assets/ScenarioBudget.stories-iIB7eJQO.js
@@ -0,0 +1,5 @@
+import{m as a}from"./budget.data-CxyOp5YF.js";import{S as m}from"./TimelineScrubber.component-BEFcJqRm.js";import"./ScenarioGraph.component-DZrrT_q8.js";import"./jsx-runtime-D_zvdyIk.js";import"./index-PahMPgEy.js";import"./colors-Cuw9ST8W.js";import"./CollapsibleTree.component-DSHTWi39.js";import"./Fire.icon-Di-S4vvp.js";import"./Header.component-BGqI9wSV.js";import"./react-resizable-panels.browser.esm-zIh8wNIw.js";const S={component:m,parameters:{layout:"fullscreen"}},r={args:{budget:a}};var o,e,t;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    budget: mockCategoriesForScenarioOne
+  }
+}`,...(t=(e=r.parameters)==null?void 0:e.docs)==null?void 0:t.source}}};const O=["Default"];export{r as Default,O as __namedExportsOrder,S as default};
