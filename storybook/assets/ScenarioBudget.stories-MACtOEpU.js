ba3ebeab37ddb7c9ceeced9ce90d47cc87391523
Serge Krstic <srdjankrstic@gmail.com>
2025-01-14 11:22:07 +0800

    Publish app and storybook

---
+++ storybook/assets/ScenarioBudget.stories-MACtOEpU.js
@@ -0,0 +1,5 @@
+import{m as a}from"./budget.data-6qZmc1wy.js";import{S as m}from"./TimelineScrubber.component-LLz_8pEk.js";import"./ScenarioGraph.component-8XjDygki.js";import"./jsx-runtime-D_zvdyIk.js";import"./index-PahMPgEy.js";import"./colors-Cuw9ST8W.js";import"./CollapsibleTree.component-DSHTWi39.js";import"./Fire.icon-Di-S4vvp.js";import"./Header.component-BGqI9wSV.js";import"./react-resizable-panels.browser.esm-zIh8wNIw.js";const b={component:m,parameters:{layout:"fullscreen"}},r={args:{budget:a.job1}};var o,e,t;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    budget: mockBudgetCategoriesMap['job1']
+  }
+}`,...(t=(e=r.parameters)==null?void 0:e.docs)==null?void 0:t.source}}};const B=["Default"];export{r as Default,B as __namedExportsOrder,b as default};
