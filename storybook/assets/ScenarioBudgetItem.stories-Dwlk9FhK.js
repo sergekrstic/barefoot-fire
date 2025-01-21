38eb5fd62e520134cb9445c8168e1ca4bd085477
Serge Krstic <srdjankrstic@gmail.com>
2025-01-21 14:55:37 +0800

    Publish app and storybook

---
+++ storybook/assets/ScenarioBudgetItem.stories-Dwlk9FhK.js
@@ -0,0 +1,31 @@
+import{a as s}from"./TimelineScrubber.component-B0Y13nkF.js";import"./jsx-runtime-D_zvdyIk.js";import"./CollapsibleTree.component-30vHkhqS.js";import"./index-PahMPgEy.js";import"./DropdownMenu.component-2YZdI-ss.js";import"./index-jVKrJXHJ.js";import"./index-Dk70umWa.js";import"./ScenarioGraph.component-Dv1Z6XQf.js";import"./colors-Cuw9ST8W.js";import"./react-resizable-panels.browser.esm-zIh8wNIw.js";const I={component:s,parameters:{layout:"fullscreen"}},e={args:{type:"parent",budget:{id:"1",name:"Parent Item",amount:1e3,frequency:"month",startDate:"2021-01-01",endDate:"2021-12-31"},onUpdateName:()=>{},onUpdateAmount:()=>{},onDelete:()=>{}}},t={args:{type:"leaf",budget:{id:"2",name:"Leaf Item",amount:-500,frequency:"month",startDate:"2021-01-01",endDate:"2021-12-31"},onUpdateName:()=>{},onUpdateAmount:()=>{},onDelete:()=>{}}};var n,a,r;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    type: 'parent',
+    budget: {
+      id: '1',
+      name: 'Parent Item',
+      amount: 1000,
+      frequency: 'month',
+      startDate: '2021-01-01',
+      endDate: '2021-12-31'
+    },
+    onUpdateName: () => {},
+    onUpdateAmount: () => {},
+    onDelete: () => {}
+  }
+}`,...(r=(a=e.parameters)==null?void 0:a.docs)==null?void 0:r.source}}};var o,m,p;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    type: 'leaf',
+    budget: {
+      id: '2',
+      name: 'Leaf Item',
+      amount: -500,
+      frequency: 'month',
+      startDate: '2021-01-01',
+      endDate: '2021-12-31'
+    },
+    onUpdateName: () => {},
+    onUpdateAmount: () => {},
+    onDelete: () => {}
+  }
+}`,...(p=(m=t.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};const b=["Parent","Leaf"];export{t as Leaf,e as Parent,b as __namedExportsOrder,I as default};
---
+++ storybook/assets/ScenarioGraph.stories-DJOvfuDH.js
@@ -0,0 +1,8 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{S as n,b as p}from"./ScenarioGraph.component-Dv1Z6XQf.js";import"./index-PahMPgEy.js";import"./colors-Cuw9ST8W.js";const l={component:n,parameters:{layout:"fullscreen"}},r={args:{data:p},render:t=>e.jsx("div",{className:"h-screen w-screen",children:e.jsx(n,{...t})})};var a,s,o;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    data: simpleScenarioGraph
+  },
+  render: props => <div className="h-screen w-screen">
+      <ScenarioGraph {...props} />
+    </div>
+}`,...(o=(s=r.parameters)==null?void 0:s.docs)==null?void 0:o.source}}};const u=["Default"];export{r as Default,u as __namedExportsOrder,l as default};
