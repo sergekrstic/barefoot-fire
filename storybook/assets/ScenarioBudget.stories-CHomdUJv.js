ccfbf4899fd8243d28b8146dc3778b851f964379
Serge Krstic <srdjankrstic@gmail.com>
2025-01-23 18:56:31 +0800

    Publish app and storybook

---
+++ storybook/assets/ScenarioBudget.stories-CHomdUJv.js
@@ -0,0 +1,7 @@
+import{s as t}from"./ScenarioGraph.component-CRAv5duO.js";import{S as m}from"./TimelineScrubber.component-c3zVx--a.js";import"./jsx-runtime-D_zvdyIk.js";import"./index-PahMPgEy.js";import"./clsx-B-dksMZM.js";import"./colors-Cuw9ST8W.js";import"./CollapsibleTree.component-DRXLFRFl.js";import"./Menu.icon-Dh1hQpZk.js";import"./DatePicker.component-CgTLAvva.js";import"./index-jVKrJXHJ.js";import"./index-Dk70umWa.js";import"./DropdownMenu.component-2YZdI-ss.js";import"./react-resizable-panels.browser.esm-zIh8wNIw.js";const h={component:m,parameters:{layout:"fullscreen"}},r={args:{scenario:t.job1,onAddBranch:()=>{},onUpdateScenarioName:()=>{}}};var o,a,e;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    scenario: simpleScenarioMap['job1'],
+    onAddBranch: () => {},
+    onUpdateScenarioName: () => {}
+  }
+}`,...(e=(a=r.parameters)==null?void 0:a.docs)==null?void 0:e.source}}};const j=["Default"];export{r as Default,j as __namedExportsOrder,h as default};
---
+++ storybook/assets/ScenarioBudgetItem.stories-BtApITgH.js
@@ -0,0 +1,31 @@
+import{a as s}from"./TimelineScrubber.component-c3zVx--a.js";import"./jsx-runtime-D_zvdyIk.js";import"./CollapsibleTree.component-DRXLFRFl.js";import"./index-PahMPgEy.js";import"./Menu.icon-Dh1hQpZk.js";import"./DatePicker.component-CgTLAvva.js";import"./index-jVKrJXHJ.js";import"./index-Dk70umWa.js";import"./clsx-B-dksMZM.js";import"./DropdownMenu.component-2YZdI-ss.js";import"./ScenarioGraph.component-CRAv5duO.js";import"./colors-Cuw9ST8W.js";import"./react-resizable-panels.browser.esm-zIh8wNIw.js";const q={component:s,parameters:{layout:"fullscreen"}},e={args:{type:"parent",budget:{id:"1",name:"Parent Item",amount:1e3,frequency:"month",startDate:"2021-01-01",endDate:"2021-12-31"},onUpdateName:()=>{},onUpdateAmount:()=>{},onDelete:()=>{}}},t={args:{type:"leaf",budget:{id:"2",name:"Leaf Item",amount:-500,frequency:"month",startDate:"2021-01-01",endDate:"2021-12-31"},onUpdateName:()=>{},onUpdateAmount:()=>{},onDelete:()=>{}}};var n,a,r;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    type: 'parent',
+    budget: {
+      id: '1',
+      name: 'Parent Item',
+      amount: 1000,
+      frequency: 'month',
+      startDate: '2021-01-01',
+      endDate: '2021-12-31'
+    },
+    onUpdateName: () => {},
+    onUpdateAmount: () => {},
+    onDelete: () => {}
+  }
+}`,...(r=(a=e.parameters)==null?void 0:a.docs)==null?void 0:r.source}}};var o,m,p;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    type: 'leaf',
+    budget: {
+      id: '2',
+      name: 'Leaf Item',
+      amount: -500,
+      frequency: 'month',
+      startDate: '2021-01-01',
+      endDate: '2021-12-31'
+    },
+    onUpdateName: () => {},
+    onUpdateAmount: () => {},
+    onDelete: () => {}
+  }
+}`,...(p=(m=t.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};const A=["Parent","Leaf"];export{t as Leaf,e as Parent,A as __namedExportsOrder,q as default};
