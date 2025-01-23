22c2a2f3ee0b6161975663e297373c4c6ad52e34
Serge Krstic <srdjankrstic@gmail.com>
2025-01-23 13:38:24 +0800

    Publish app and storybook

---
+++ storybook/assets/DatePicker.stories-BWpQhvmd.js
@@ -0,0 +1,10 @@
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{D as i}from"./DatePicker.component-D_nLB7of.js";import"./Menu.icon-Dh1hQpZk.js";import"./index-PahMPgEy.js";import"./index-jVKrJXHJ.js";import"./index-Dk70umWa.js";import"./clsx-B-dksMZM.js";const j={component:i},r={args:{value:new Date}},e={args:r.args,render:()=>t.jsx("div",{children:t.jsx("input",{type:"date"})})};var a,s,o;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    value: new Date()
+  }
+}`,...(o=(s=r.parameters)==null?void 0:s.docs)==null?void 0:o.source}}};var n,m,p;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: Custom.args,
+  render: () => <div>
+      <input type="date" />
+    </div>
+}`,...(p=(m=e.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};const D=["Custom","Native"];export{r as Custom,e as Native,D as __namedExportsOrder,j as default};
--- storybook/assets/ScenarioBudget.stories-Dm74MeTI.js
+++
@@ -1,7 +0,0 @@
-import{s as t}from"./ScenarioGraph.component-D4ACKses.js";import{S as n}from"./TimelineScrubber.component-DObUASMl.js";import"./jsx-runtime-D_zvdyIk.js";import"./index-PahMPgEy.js";import"./colors-Cuw9ST8W.js";import"./CollapsibleTree.component-30vHkhqS.js";import"./DropdownMenu.component-2YZdI-ss.js";import"./index-jVKrJXHJ.js";import"./index-Dk70umWa.js";import"./react-resizable-panels.browser.esm-zIh8wNIw.js";const g={component:n,parameters:{layout:"fullscreen"}},r={args:{scenario:t.job1,onAddBranch:()=>{},onUpdateScenarioName:()=>{}}};var o,a,e;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {
-    scenario: simpleScenarioMap['job1'],
-    onAddBranch: () => {},
-    onUpdateScenarioName: () => {}
-  }
-}`,...(e=(a=r.parameters)==null?void 0:a.docs)==null?void 0:e.source}}};const B=["Default"];export{r as Default,B as __namedExportsOrder,g as default};
