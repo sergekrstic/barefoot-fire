4a1d55d1bdc3267192596f67cf4ae6e74428a4fc
Serge Krstic <srdjankrstic@gmail.com>
2025-01-25 19:12:45 +0800

    Publish app and storybook

--- storybook/assets/ScenarioBudget.stories-zEHbDiO9.js
+++
@@ -1,7 +0,0 @@
-import{s as t}from"./ScenarioGraph.component-CRAv5duO.js";import{S as m}from"./TimelineScrubber.component-DWTkBeZi.js";import"./jsx-runtime-D_zvdyIk.js";import"./index-PahMPgEy.js";import"./clsx-B-dksMZM.js";import"./colors-Cuw9ST8W.js";import"./CollapsibleTree.component-DRXLFRFl.js";import"./Menu.icon-Dh1hQpZk.js";import"./DatePicker.component-BJiRWqV-.js";import"./floating-ui.react-f02HS2BF.js";import"./index-jVKrJXHJ.js";import"./index-Dk70umWa.js";import"./DropdownMenu.component-BYo-Av9w.js";import"./react-resizable-panels.browser.esm-zIh8wNIw.js";const j={component:m,parameters:{layout:"fullscreen"}},r={args:{scenario:t.job1,onAddBranch:()=>{},onUpdateScenarioName:()=>{}}};var o,a,e;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {
-    scenario: simpleScenarioMap['job1'],
-    onAddBranch: () => {},
-    onUpdateScenarioName: () => {}
-  }
-}`,...(e=(a=r.parameters)==null?void 0:a.docs)==null?void 0:e.source}}};const x=["Default"];export{r as Default,x as __namedExportsOrder,j as default};
---
+++ storybook/assets/TimelineScrubber.stories-B3hNeQln.js
@@ -0,0 +1,19 @@
+import{g as u}from"./ScenarioGraph.component-CRAv5duO.js";import{T as l}from"./TimelineScrubber.component-Bye3qQj4.js";import"./jsx-runtime-D_zvdyIk.js";import"./index-PahMPgEy.js";import"./clsx-B-dksMZM.js";import"./colors-Cuw9ST8W.js";import"./CollapsibleTree.component-DRXLFRFl.js";import"./Menu.icon-Dh1hQpZk.js";import"./ContextMenu.component-CiBhxQiZ.js";import"./floating-ui.react-DXVSm5aW.js";import"./index-jVKrJXHJ.js";import"./index-Dk70umWa.js";import"./DatePicker.component-D4qOE7Yf.js";import"./DropdownMenu.component-wXtweCx3.js";import"./Modal.component-BzCUJgMo.js";import"./Popover.trigger-CTMGeKFO.js";import"./Tooltip.trigger-BLwQiu7C.js";import"./react-resizable-panels.browser.esm-zIh8wNIw.js";const k={component:l,parameters:{layout:"fullscreen"}},e={args:{type:"area",data:[],selection:[0,100]}},a={args:{type:"area",data:u(),selection:[0,100]}},r={args:{type:"area",data:u(),selection:[25,75]}};var t,o,s;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    type: 'area',
+    data: [],
+    selection: [0, 100]
+  }
+}`,...(s=(o=e.parameters)==null?void 0:o.docs)==null?void 0:s.source}}};var m,n,i;a.parameters={...a.parameters,docs:{...(m=a.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    type: 'area',
+    data: generateRandomTimeSeriesData(),
+    selection: [0, 100]
+  }
+}`,...(i=(n=a.parameters)==null?void 0:n.docs)==null?void 0:i.source}}};var p,c,d;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    type: 'area',
+    data: generateRandomTimeSeriesData(),
+    selection: [25, 75]
+  }
+}`,...(d=(c=r.parameters)==null?void 0:c.docs)==null?void 0:d.source}}};const q=["NoData","WithDataZoomedOut","WithDataZoomedIn"];export{e as NoData,r as WithDataZoomedIn,a as WithDataZoomedOut,q as __namedExportsOrder,k as default};
