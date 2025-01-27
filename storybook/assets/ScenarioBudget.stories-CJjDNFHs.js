7f0215df50c3512918d3ee7e78ee31ba0e5eed0d
Serge Krstic <srdjankrstic@gmail.com>
2025-01-27 11:40:12 +0800

    Publish app and storybook

--- storybook/assets/ScenarioBudget.stories-CJjDNFHs.js
+++
@@ -1,7 +0,0 @@
-import{s as t}from"./ScenarioGraph.component-CRAv5duO.js";import{S as m}from"./TimelineScrubber.component-Bye3qQj4.js";import"./jsx-runtime-D_zvdyIk.js";import"./index-PahMPgEy.js";import"./clsx-B-dksMZM.js";import"./colors-Cuw9ST8W.js";import"./CollapsibleTree.component-DRXLFRFl.js";import"./Menu.icon-Dh1hQpZk.js";import"./ContextMenu.component-CiBhxQiZ.js";import"./floating-ui.react-DXVSm5aW.js";import"./index-jVKrJXHJ.js";import"./index-Dk70umWa.js";import"./DatePicker.component-D4qOE7Yf.js";import"./DropdownMenu.component-wXtweCx3.js";import"./Modal.component-BzCUJgMo.js";import"./Popover.trigger-CTMGeKFO.js";import"./Tooltip.trigger-BLwQiu7C.js";import"./react-resizable-panels.browser.esm-zIh8wNIw.js";const M={component:m,parameters:{layout:"fullscreen"}},r={args:{scenario:t.job1,onAddBranch:()=>{},onUpdateScenarioName:()=>{}}};var o,a,e;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {
-    scenario: simpleScenarioMap['job1'],
-    onAddBranch: () => {},
-    onUpdateScenarioName: () => {}
-  }
-}`,...(e=(a=r.parameters)==null?void 0:a.docs)==null?void 0:e.source}}};const N=["Default"];export{r as Default,N as __namedExportsOrder,M as default};
---
+++ storybook/assets/ScenarioBudget.stories-Dsmb-XBr.js
@@ -0,0 +1,8 @@
+import{s as e}from"./ScenarioGraph.component-CRBOjlcj.js";import{S as p}from"./TimelineScrubber.component-ut3e5M2m.js";import"./jsx-runtime-D_zvdyIk.js";import"./index-PahMPgEy.js";import"./clsx-B-dksMZM.js";import"./colors-Cuw9ST8W.js";import"./CollapsibleTree.component-DRXLFRFl.js";import"./Menu.icon-Dh1hQpZk.js";import"./ContextMenu.component-CiBhxQiZ.js";import"./floating-ui.react-DXVSm5aW.js";import"./index-jVKrJXHJ.js";import"./index-Dk70umWa.js";import"./DatePicker.component-D4qOE7Yf.js";import"./DropdownMenu.component-wXtweCx3.js";import"./Modal.component-BzCUJgMo.js";import"./Popover.trigger-CTMGeKFO.js";import"./Tooltip.trigger-BLwQiu7C.js";import"./react-resizable-panels.browser.esm-zIh8wNIw.js";const A={component:p,parameters:{layout:"fullscreen"}},r={args:{scenario:e.job1,onAddBranch:()=>{},onUpdateScenarioName:()=>{},onUpdateScenarioStartDate:()=>{}}};var o,a,t;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    scenario: simpleScenarioMap['job1'],
+    onAddBranch: () => {},
+    onUpdateScenarioName: () => {},
+    onUpdateScenarioStartDate: () => {}
+  }
+}`,...(t=(a=r.parameters)==null?void 0:a.docs)==null?void 0:t.source}}};const M=["Default"];export{r as Default,M as __namedExportsOrder,A as default};
