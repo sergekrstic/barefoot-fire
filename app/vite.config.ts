f1e218fecad3a15707a408a5ff3912561623be0f
Serge Krstic <srdjankrstic@gmail.com>
2025-01-30 11:33:58 +0800

    Update npm packages

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,18 +1,18 @@
-import MillionLint from '@million/lint'
-// import react from '@vitejs/plugin-react-swc'
-import react from '@vitejs/plugin-react'
+// import MillionLint from '@million/lint'
+import react from '@vitejs/plugin-react-swc'
+// import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-const ReactCompilerConfig = {}
+// const ReactCompilerConfig = {}
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
-    MillionLint.vite({ enabled: false }),
-    react({ babel: { plugins: [['babel-plugin-react-compiler', ReactCompilerConfig]] } }),
-    // react().
+    // MillionLint.vite({ enabled: false }),
+    // react({ babel: { plugins: [['babel-plugin-react-compiler', ReactCompilerConfig]] } }),
+    react(),
   ],
   esbuild: { jsx: 'automatic' },
 })
