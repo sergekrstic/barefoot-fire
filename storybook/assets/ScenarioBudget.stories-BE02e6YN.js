4792637a64eb4c1143de5f6edfac32c6a3b9c61d
Serge Krstic <srdjankrstic@gmail.com>
2025-06-06 14:41:42 +1000

    Publish app and storybook

---
+++ storybook/assets/ScenarioBudget.stories-BE02e6YN.js
@@ -0,0 +1,12 @@
+import{s as n}from"./ScenarioGraph.component-BMugaKV0.js";import{S as a}from"./TimelineScrubber.component-Bt_S_518.js";import"./moment-CGDQJbyj.js";import"./clsx-B-dksMZM.js";import"./jsx-runtime-CLpGMVip.js";import"./index-BBGmM4Ve.js";import"./colors-CdXc3yLe.js";import"./CollapsibleTreeNode.component-Cokz7cxH.js";import"./Menu.icon-B-AbzxGV.js";import"./ContextMenu.component-C5rbgKDU.js";import"./floating-ui.react-40E8SCbq.js";import"./index-DNXr5OWd.js";import"./index-CnEmR3yp.js";import"./DatePicker.component-BGinrmpb.js";import"./DropdownMenu.component-BiHVZO5i.js";import"./Modal.component-Bi2H5BKg.js";import"./Popover.trigger-D7H7D4Fb.js";import"./Select.component-D-qpFJd6.js";import"./Tooltip.trigger-Cx9X1lyG.js";import"./react-resizable-panels.browser.esm-D8Tkf0_w.js";const j={component:a,parameters:{layout:"fullscreen"}},o={args:{scenario:n.job1,rollupFrequency:"monthly",onSelectRollupFrequency:()=>{},onAddBranch:()=>{},onUpdateScenarioName:()=>{},onUpdateScenarioStartDate:()=>{},onDeleteScenario:()=>{},onAddBudget:()=>{}}};var e,r,t;o.parameters={...o.parameters,docs:{...(e=o.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    scenario: simpleScenarioMap['job1'],
+    rollupFrequency: 'monthly',
+    onSelectRollupFrequency: () => {},
+    onAddBranch: () => {},
+    onUpdateScenarioName: () => {},
+    onUpdateScenarioStartDate: () => {},
+    onDeleteScenario: () => {},
+    onAddBudget: () => {}
+  }
+}`,...(t=(r=o.parameters)==null?void 0:r.docs)==null?void 0:t.source}}};const x=["Default"];export{o as Default,x as __namedExportsOrder,j as default};
---
+++ storybook/assets/ScenarioGraph.stories-BerLMUdw.js
@@ -0,0 +1,8 @@
+import{j as e}from"./jsx-runtime-CLpGMVip.js";import{S as n,a as p}from"./ScenarioGraph.component-BMugaKV0.js";import"./moment-CGDQJbyj.js";import"./clsx-B-dksMZM.js";import"./index-BBGmM4Ve.js";import"./colors-CdXc3yLe.js";const h={component:n,parameters:{layout:"fullscreen"}},r={args:{data:p},render:t=>e.jsx("div",{className:"h-screen w-screen",children:e.jsx(n,{...t})})};var a,s,o;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    data: simpleScenarioGraph
+  },
+  render: props => <div className="h-screen w-screen">
+      <ScenarioGraph {...props} />
+    </div>
+}`,...(o=(s=r.parameters)==null?void 0:s.docs)==null?void 0:o.source}}};const f=["Default"];export{r as Default,f as __namedExportsOrder,h as default};
